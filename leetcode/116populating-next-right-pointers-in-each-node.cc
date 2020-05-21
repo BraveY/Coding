9de40ffc48e9c3f5d8e4d87119c9ce6f13b68ee4
BraveY <lsz_yky@163.com>
2020-05-21 19:59:32 +0800

    new solutions 2020-05-21 19:59:32

---
+++ leetcode/116populating-next-right-pointers-in-each-node.cc
@@ -0,0 +1,85 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Node {
+ public:
+	int val;
+	Node* left;
+	Node* right;
+	Node* next;
+
+	Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+	Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+	Node(int _val, Node* _left, Node* _right, Node* _next)
+		: val(_val), left(_left), right(_right), next(_next) {}
+};
+
+/*
+Runtime: 28 ms, faster than 65.60% of C++ online submissions for Populating Next Right Pointers in Each Node.
+Memory Usage: 16.7 MB, less than 100.00% of C++ online submissions for Populating Next Right Pointers in Each Node.
+ */
+class Solution {
+ public:
+	Node* connect(Node* root) {
+		if (!root) return nullptr;
+		if (!root->left) return root;
+		root->left->next = root->right;
+		if (root->next)
+			root->right->next = root->next->left;
+		connect(root->left);
+		connect(root->right);
+		return root;
+	}
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	// cout << sol.solution(nums2) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<char>> grid(m, vector<char>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
