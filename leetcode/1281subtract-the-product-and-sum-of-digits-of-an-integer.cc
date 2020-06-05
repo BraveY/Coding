3912b5a8b37592b614f4c586783cdea6b37254c8
BraveY <lsz_yky@163.com>
2020-06-05 21:54:44 +0800

    new solutions 2020-06-05 21:54:44

---
+++ leetcode/1281subtract-the-product-and-sum-of-digits-of-an-integer.cc
@@ -0,0 +1,74 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Subtract the Product and Sum of Digits of an Integer.
+Memory Usage: 6.1 MB, less than 32.38% of C++ online submissions for Subtract the Product and Sum of Digits of an Integer.
+ */
+class Solution {
+  public:
+	int subtractProductAndSum(int n) {
+		int nums = n;
+		int product = 1;
+		int sum = 0;
+		while (nums) {
+			int left = (nums % 10);
+			product *= left;
+			sum += left;
+			nums = nums / 10 ;
+		}
+		return product - sum;
+	}
+
+  private:
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	cout << sol.subtractProductAndSum(234) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<int>> grid(m, vector<int>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
