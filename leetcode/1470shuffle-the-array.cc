162bfb4a3d25f668ffd713f090340191ebf998c4
BraveY <lsz_yky@163.com>
2020-06-09 21:56:35 +0800

    new solutions 2020-06-09 21:56:35

---
+++ leetcode/1470shuffle-the-array.cc
@@ -0,0 +1,70 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/*
+Runtime: 12 ms, faster than 74.53% of C++ online submissions for Shuffle the Array.
+Memory Usage: 10.1 MB, less than 100.00% of C++ online submissions for Shuffle the Array.
+ */
+class Solution {
+  public:
+	vector<int> shuffle(vector<int>& nums, int n) {
+		vector<int> ans;
+		for (int i = 0; i < n; i++) {
+			ans.push_back(nums[i]);
+			ans.push_back(nums[i + n]);
+		}
+		return ans;
+	}
+
+  private:
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	// cout << sol.solution(nums2) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<int>> grid(m, vector<int>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
