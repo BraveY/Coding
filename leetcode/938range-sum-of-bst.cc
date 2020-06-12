a1b2c605875a181e00f82d84018a7265d66746c5
BraveY <lsz_yky@163.com>
2020-06-12 09:42:34 +0800

    new solutions 2020-06-12 09:42:34

---
+++ leetcode/938range-sum-of-bst.cc
@@ -0,0 +1,69 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+#include "../binarytree.h"
+using namespace std;
+
+class Solution {
+  public:
+	int rangeSumBST(TreeNode* root, int L, int R) {
+		return DFS(root, L, R);
+	}
+
+  private:
+	int DFS(TreeNode* root, int L, int R) {
+		if (!root) return 0;
+		int sum = 0;
+		if (root->val >= L && root->val <= R) sum += root->val;
+		sum += DFS(root->left, L, R);
+		sum += DFS(root->right, L, R);
+		return sum;
+	}
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	// cout << sol.solution(nums2) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<int>> grid(m, vector<int>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
