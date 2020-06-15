e9d46d7b90c0dca4b380ea76cbb4b2684569891a
BraveY <lsz_yky@163.com>
2020-06-15 09:53:43 +0800

    new solutions 2020-06-15 09:53:43

---
+++ leetcode/1480running-sum-of-1d-array.cc
@@ -0,0 +1,72 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/*
+Runtime: 4 ms, faster than 80.00% of C++ online submissions for Running Sum of 1d Array.
+Memory Usage: 8.7 MB, less than 20.00% of C++ online submissions for Running Sum of 1d Array.
+ */
+class Solution {
+  public:
+	vector<int> runningSum(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> ans(n, 0);
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += nums[i];
+			ans[i] = sum;
+		}
+		return ans;
+	}
+
+  private:
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	// cout << sol.solution(nums2) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<int>> grid(m, vector<int>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
