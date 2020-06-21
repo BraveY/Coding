9294725dc8b9f15ef808e843b2bded31656df953
BraveY <lsz_yky@163.com>
2020-06-21 15:15:18 +0800

    new solutions 2020-06-21 15:15:18

---
+++ leetcode/1486xor-operation-in-an-array.cc
@@ -0,0 +1,71 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for XOR Operation in an Array.
+Memory Usage: 5.9 MB, less than 100.00% of C++ online submissions for XOR Operation in an Array.
+ */
+class Solution {
+  public:
+	int xorOperation(int n, int start) {
+		int ans = start;
+		int cur;
+		for (int i = 1; i < n; i++) {
+			cur = start + 2 * i;
+			ans = ans ^ cur;
+		}
+		return ans;
+	}
+
+  private:
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	// cout << sol.solution(nums2) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<int>> grid(m, vector<int>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
