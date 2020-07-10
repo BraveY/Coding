12dabef3ff99788f25e36837e91efe4a320e46d3
BraveY <lsz_yky@163.com>
2020-07-10 22:06:12 +0800

    new solutions 2020-07-10 22:06:12

---
+++ leetcode/700search-in-a-binary-search-tree.cc
@@ -0,0 +1,68 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+#include "../binarytree.h"
+
+using namespace std;
+
+/*
+Runtime: 60 ms, faster than 89.64% of C++ online submissions for Search in a Binary Search Tree.
+Memory Usage: 34.7 MB, less than 85.18% of C++ online submissions for Search in a Binary Search Tree.
+ */
+class Solution {
+  public:
+	TreeNode* searchBST(TreeNode* root, int val) {
+		if (!root) return NULL;
+		if (root->val > val )  return searchBST(root->left, val);
+		else if (root->val < val) return searchBST(root->right, val);
+		else return root;
+	}
+  private:
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	// cout << sol.solution(nums2) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<int>> grid(m, vector<int>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
