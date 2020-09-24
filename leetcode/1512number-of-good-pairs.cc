b872dd7d1906d8c395902ed2a5f67cfc57acdf9e
BraveY <lsz_yky@163.com>
2020-09-24 22:18:22 +0800

    new solutions 2020-09-24 22:18:22

---
+++ leetcode/1512number-of-good-pairs.cc
@@ -0,0 +1,74 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+/*
+Runtime: 4 ms, faster than 50.61% of C++ online submissions for Number of Good Pairs.
+Memory Usage: 7.4 MB, less than 62.40% of C++ online submissions for Number of Good
+ */
+class Solution {
+  public:
+	int numIdenticalPairs(vector<int>& nums) {
+		vector<int> dict(101, 0);
+		int ans = 0;
+		for (int i = 0; i < nums.size(); ++i) {
+			if (!dict[nums[i]]) dict[nums[i]]++;
+			else {
+				ans += dict[nums[i]]++;
+			}
+		}
+		return ans;
+	}
+
+  private:
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	// cout << sol.solution(nums2) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<int>> grid(m, vector<int>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
