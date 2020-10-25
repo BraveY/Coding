07dfda191cfcea85fb9ef90af75e2104f0282dea
BraveY <lsz_yky@163.com>
2020-10-25 15:28:42 +0800

    new solutions 2020-10-25 15:28:42

---
+++ leetcode/338counting-bits.cc
@@ -0,0 +1,69 @@
+#include <limits.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/*
+Runtime: 8 ms, faster than 70.04% of C++ online submissions for Counting Bits.
+Memory Usage: 8.3 MB, less than 35.13% of C++ online submissions for Counting Bits.
+ */
+class Solution {
+  public:
+	vector<int> countBits(int num) {
+		vector<int> ans(num + 1, 0);
+		for (int i = 1; i <= num; ++i) {
+			ans[i] = ans[i & (i - 1)] + 1;
+		}
+		return ans;
+	}
+
+  private:
+};
+
+void cout_vector(vector<int>& nums) {
+	for (auto i : nums) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+void debug() {
+	Solution sol;
+
+	//一维数组的输入
+	// vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+	// vector<int> nums2;
+	// int i;
+	// while (cin >> i) {
+	// 	nums2.push_back(i);
+	// }
+	// cout << sol.solution(nums2) << endl;
+
+	//二维数组的输入
+	//int m, n;
+	// while (cin >> m >> n) {
+	// 	vector<vector<int>> grid(m, vector<int>(n));
+	// 	for (int i = 0; i < m; i++) {
+	// 		for (int j = 0; j < n; j++ ) {
+	// 			cin >> grid[i][j];
+	// 		}
+	// 	}
+	// 	int ans = sol.solution(grid);
+	// 	cout << ans << endl;
+	// }
+	return;
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	ios::sync_with_stdio(false);
+	debug();
+	system("pause");
+	return 0;
+}
